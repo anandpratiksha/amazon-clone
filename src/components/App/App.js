9aab860bc2374fecd03042a866801bc67ea28f49
anandpratiksha <apratiksha1137@gmail.com>
2021-09-29 15:46:05 +0530

    updates

---
+++ src/components/App/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Login from '../Login/Login';
+import { BrowserRouter as Router, Switch, Route }
+  from "react-router-dom";
+import Checkout from '../Checkout/Checkout';
+import { auth } from "../../firebase/firebase";
+import { useStateValue } from "../../StateProvider/StateProvider";
+
+function App() {
+  const [{user}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS ", authUser);
+      if (authUser) { //the user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {  //the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+    return () => {
+      unsubscribe();
+    }
+  }, []);
+
+  return (
+
+// route renders UI when its path matches the URL
+
+    
+    // Link  anchor  tag navigation around the app
+    
+    <Router>
+      <div className="app">
+
+        {/* switch renders first route that matches location rather thn rendering ALL routes */}
+        
+        <Switch>
+          {/* will go to login page */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+        {/* will directly go to main page */}
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
